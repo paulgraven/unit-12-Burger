fb1c7e7a81f876802e0cc9a33492b28a46132e7f
Paul Graven <graven.paul@gmail.com>
2019-09-27 16:34:35 -0600

    updated all. Need to update css

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Cheeseburger', false),
+    ('Bacon Burger', false),
+    ('Bacon Cheeseburger', false),
+    ('Avocado Burger', false),
+    ('California Burger', false),
+    ('Bison Burger', false);
